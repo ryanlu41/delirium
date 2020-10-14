17a5d05a68c7109bf5d4918cf8473be33f9542b5
supatuffpinkpuff <kirbygong@gmail.com>
2020-10-14 15:21:46 -0400

    Delete DeliriumOnsetTimes.sql

--- First 24 Hour/Dataset/DeliriumOnsetTimes.sql
+++
@@ -1,6 +0,0 @@
-﻿CREATE TABLE "DeliriumOnsetTimes" AS 
-SELECT "patientunitstayid"
-	,MIN("nursingchartoffset")
-FROM "eicu_crd"."DeliriumTimeStamps" 
-GROUP BY patientunitstayid
-ORDER BY patientunitstayid
