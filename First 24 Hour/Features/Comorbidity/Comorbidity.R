d37e1376401784542aa73fda4fa1803c085a922a
akaashsanyal <akaashsanyal@gmail.com>
2021-01-09 12:13:29 -0500

    Update Comorbidity.R

--- First 24 Hour/Features/Comorbidity/Comorbidity.R
+++ First 24 Hour/Features/Comorbidity/Comorbidity.R
@@ -19,18 +19,9 @@ icd9_codes <- separate(data = relevant_diagnosis, col = icd9code, into = c("icd9
   filter(icd9 != "") %>%
   drop_na
 icd9_codes <- icd9_codes[order(icd9_codes$patientunitstayid), ]
-icd10_codes <- separate(data = relevant_diagnosis, col = icd9code, into = c("icd9", "icd10"), sep = ",") %>%
-  select(patientunitstayid, icd10) %>%
-  filter(icd10 != "") %>%
-  drop_na
-icd10_codes <- icd10_codes[order(icd10_codes$patientunitstayid), ]
 
 # Generate Elixhauser Comorbidity Index
 elixhauser_icd9 <- comorbidity(x = icd9_codes, id = "patientunitstayid", code = "icd9", score = "elixhauser", icd = "icd9", assign0 = FALSE)
-elixhauser_icd10 <- comorbidity(x = icd10_codes, id = "patientunitstayid", code = "icd10", score = "elixhauser", icd = "icd10", assign0 = FALSE)
-
-# Find which patients only have comorbidity indices from ICD9
-missing = elixhauser_icd9[which(elixhauser_icd9$patientunitstayid %notin% elixhauser_icd10$patientunitstayid), ]
 
 # Save to csv
 write.csv(elixhauser_icd9, 'complete_24hr_comorbidity.csv')
