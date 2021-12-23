fcc010e640878dc3b1bab1c1b286613439b9ef72
supatuffpinkpuff <kirbygong@gmail.com>
2021-12-23 17:42:48 -0500

    relative filepathing

--- First 24 Hour/Features/Ventilation/VentilationBinary24Hours.py
+++ First 24 Hour/Features/Ventilation/VentilationBinary24Hours.py
@@ -3,31 +3,38 @@
 Created on Sun Apr 26 15:25:10 2020
 
 This code pulls for the prediction/clustering patients whether or not they were 
-documented as receiving ventilation during their ICU stay. Checked the 
-apacheApsVar, PhysicalExam, or Treatment tables. 
+documented as receiving ventilation during their ICU stay in the 1st 24 hours. 
+Checked the apacheApsVar, PhysicalExam, or Treatment tables. 
 
 CarePlanGeneral, and respiratory care are less reliable, so not using them.
+
+Run time: 15 seconds
 
 @author: Kirby
 """
 #%% Package setup.
 import numpy as np
 import pandas as pd
+from pathlib import Path
+
+file_path = Path(__file__)
+dataset_path = file_path.parent.parent.parent.joinpath('Dataset')
+eicu_path = file_path.parent.parent.parent.parent.joinpath('eicu')
 
 #%% Load in data.
 #Load the IDs of the patients we care about.
-comp = pd.read_csv(r'C:\Users\Kirby\OneDrive\JHU\Precision Care Medicine\complete_patientstayid_list.csv')
+comp = pd.read_csv(dataset_path.joinpath('complete_patientstayid_list.csv'))
 comp.rename(columns={'PatientStayID':'patientunitstayid'},inplace=True)
 
 #Get all apacheapsvar
-apache = pd.read_csv(r"C:\Users\Kirby\OneDrive\JHU\Precision Care Medicine\eicu\apacheapsvar.csv",
+apache = pd.read_csv(eicu_path.joinpath("apacheapsvar.csv"),
                      usecols=['patientunitstayid','vent'])
 #Get physicalexam data.
-phys = pd.read_csv(r"C:\Users\Kirby\OneDrive\JHU\Precision Care Medicine\eicu\physicalexam.csv",
+phys = pd.read_csv(eicu_path.joinpath("physicalexam.csv"),
                    usecols=['patientunitstayid','physicalexamoffset',
                             'physicalexamtext'])
 #Get treatment
-treat = pd.read_csv(r"C:\Users\Kirby\OneDrive\JHU\Precision Care Medicine\eicu\treatment.csv",
+treat = pd.read_csv(eicu_path.joinpath("treatment.csv"),
                     usecols=['patientunitstayid','treatmentoffset',
                             'treatmentstring'])
 
@@ -45,13 +52,15 @@ treat = treat[treat['treatmentoffset'] >= 0]
 treat = treat[treat['treatmentoffset'] <= 1440]
 
 #Get ventilation data.
+apache = apache[apache['vent']==1]
 phys = phys[phys['physicalexamtext']=='ventilated']
 mech = treat[treat['treatmentstring'].str.contains('mechanical ventilation')]
 noninv = treat[treat['treatmentstring'].str.contains('non-invasive ventilation')]
 
 #Just get patientunitstayids that had ventilation in first 24 hours. 
 vent_ids = pd.concat([phys[['patientunitstayid']],mech[['patientunitstayid']],
-                      noninv[['patientunitstayid']]])
+                      noninv[['patientunitstayid']],
+                      apache[['patientunitstayid']]])
 vent_ids.drop_duplicates(inplace=True)
 
 comp['first24hrs_vented'] = comp['patientunitstayid'].isin(vent_ids['patientunitstayid'])
